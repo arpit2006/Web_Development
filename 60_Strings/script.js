73688f43eb1ff6a8868f1d3ca574f6a22bd39219
arpit2006 <arpitshirbhate25@gmail.com>
2025-02-12 23:02:58 +0530

    initial commit

---
+++ 60_Strings/script.js
@@ -0,0 +1,40 @@
+console.log("Strings in JS..: ")
+// let a = "Harry";
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+
+// console.log(a.length)
+
+// let name = "Arpit";
+// let friend = "Batla";
+// // console.log("His name is " + name + " and his friend is " + friend);
+
+// //This is Template Literals to make life easy of develpers
+// //When a backtick is inserted it is called string interpolation
+// console.log(`His name is ${name} and his friends name is ${friend}`);
+
+// let name = "Harru";
+// // In Built Function for strings
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.length)
+// // Slicing
+// // Syntax string.slice(starting index,ending index)
+// console.log(name.slice(0,3))
+// console.log(name.slice(1))
+
+//Replace
+// Syntax string.replace(old string,new string)
+let realname = "Arpit Shirbhate";
+console.log(realname.replace("Arpit","Archana"))
+
+//Concatination
+let name = "Arpit";
+let surname = " Shirbhate";
+// console.log("Your Full name is..: " + name +    surname)
+
+console.log(name.concat(surname)); 
+// if you create strign at start it wil same at last even after manuplating it.
